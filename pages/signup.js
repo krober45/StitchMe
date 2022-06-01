a7bd59f14af884630f785ec54cb76ae79320ceb9
Isis Alexander <99460203+QueenOfBirbs@users.noreply.github.com>
2022-06-01 13:20:39 -0700

    adding base files and experimental bootstrap stuff

---
+++ pages/signup.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SignUp = () => {
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Login
+            </Button>
+        </Form>
+    );
+};
+
+export default SignUp;
