e5b8dc3252234d526b7c1d6906791da3f8510468
Khari Robertson <kharirobertson@Kharis-MBP.fios-router.home>
2022-06-03 16:24:02 -0400

    Navbar updates

--- src/App.js
+++ src/App.js
@@ -10,21 +10,35 @@ import Themes from './pages/themes';
 import Profile from './pages/profile';
 import Project from './pages/project';
 import Update from './pages/update';
+import * as ReactBootStrap from "react-bootstrap";
   
 function App() {
 return (
-    <Router>
-    <Navbar />
-    <Routes>
-        <Route path='/aboutus' element={<AboutUs/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<SignUp/>} />
-        <Route path='/themes' element={<Themes/>} />
-        <Route path='/profile' element={<Profile/>} />
-        <Route path='/project' element={<Project/>} />
-        <Route path='/update' element={<Update/>} />
-    </Routes>
-    </Router>
+    <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <ReactBootStrap.Container>
+  <ReactBootStrap.Navbar.Brand href="#home">StitchMe</ReactBootStrap.Navbar.Brand>
+  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+    <ReactBootStrap.Nav className="me-auto">
+      <ReactBootStrap.Nav.Link href="#features">Home</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#pricing">About us</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Projects</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Profile</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Divider />
+        <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
+      </ReactBootStrap.NavDropdown>
+    </ReactBootStrap.Nav>
+    <ReactBootStrap.Nav>
+      <ReactBootStrap.Nav.Link href="#deets">Login</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
+        Sign-up
+      </ReactBootStrap.Nav.Link>
+    </ReactBootStrap.Nav>
+  </ReactBootStrap.Navbar.Collapse>
+  </ReactBootStrap.Container>
+</ReactBootStrap.Navbar>
 );
 }
   
