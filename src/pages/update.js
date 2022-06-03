56a5bf04f44b085fe3305e80f94c330efd950c46
Isis Alexander <99460203+QueenOfBirbs@users.noreply.github.com>
2022-06-03 11:57:12 -0700

    fixed signup/update/login

--- src/pages/update.js
+++ src/pages/update.js
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button'
 
 function Update() {
     const [username, setUser] = useState("");
